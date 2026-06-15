61cbd74e65cfe277ef99104903f82fb90aec05bf
samarthisgood69 <samarthisgood69@gmail.com>
2026-06-15 11:42:03 +0530

    initial commit

---
+++ logical_not.py
@@ -0,0 +1,27 @@
+# Program to check the application of logical not operator
+a=10
+b=15
+c=20
+#not is used to reverse the result of (a==b)
+print(not (a ==b))
+#not is used to reverse the result of (b==c)
+print(not(b==c))
+a = "python"
+b = "coding"
+#not is used here to check that a is not equal to b
+if not (a==b):
+      print(a, 'and', b, 'are different.')
+a = 4 
+b = 5
+#not is used here to reverse the result of compsring both the conditions
+if not ((a==1)==(b==5)):
+      print('Hello')
+a = int(input("enter a number: "))
+#not is used here to check the numebr is not even
+if not (a % 2==0):
+      print (a, "is an odd number")
+
+
+
+
+
