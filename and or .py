10632738eed0bdb29136b73d61e2a7c14e61d948
samarthisgood69 <samarthisgood69@gmail.com>
2026-06-15 11:21:26 +0530

    initial commit

---
+++ and or .py
@@ -0,0 +1,19 @@
+a = 5
+b = 5
+c = 5
+if a and b and c:
+  print("All the numbers have boolean value as true.")
+else:
+  print("atleast one number has boolean value as false.")
+
+a = 10
+b = 5
+c =-3
+if a>0 or b>0:
+     print("Either of the number is greater than zero")
+else:
+    print("No number is greatre than zero")
+if b>0 or c>0 :
+    print("Either of the number is greater than zero")
+else:
+   print("No number is greater than zero")
